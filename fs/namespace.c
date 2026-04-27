ab235527d02b4e48d1549c1b5530045d3a844898
AkinaHaruka <misaka@mis1042.com>
2026-04-27 20:16:55 +0800

    fix: Patch error when applying susfs patches

--- fs/namespace.c
+++ fs/namespace.c
@@ -1924,17 +1924,17 @@ static int can_umount(const struct path *path, int flags)
 		struct mount *mnt = real_mount(path->mnt);
 
 		if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		+		return -EINVAL;
+				return -EINVAL;
 		if (!may_mount())
-		+		return -EPERM;
+				return -EPERM;
 		if (path->dentry != path->mnt->mnt_root)
-		+		return -EINVAL;
+				return -EINVAL;
 		if (!check_mnt(mnt))
-		+		return -EINVAL;
+				return -EINVAL;
 		if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		+		return -EINVAL;
+				return -EINVAL;
 		if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		+		return -EPERM;
+				return -EPERM;
 		return 0;
 	}
 
