07d62fbd461b159ca8e661944759ad6b24585955
AkinaHaruka <misaka@mis1042.com>
2026-04-27 19:01:58 +0800

    feat: Add Hooks for ReSukiSu SUSFS

--- fs/exec.c
+++ fs/exec.c
@@ -1858,12 +1858,24 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU_SUSFS
+	__attribute__((hot))
+	extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+					void *argv, void *envp, int *flags);
+#endif
+
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+
+	#ifdef CONFIG_KSU_SUSFS
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	#endif
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1891,6 +1903,11 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+
+	#ifdef CONFIG_KSU_SUSFS // 32-bit ksud and 32-on-64 support
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	#endif
+
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
--- fs/stat.c
+++ fs/stat.c
@@ -372,12 +372,27 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 	return cp_new_stat(&stat, statbuf);
 }
 
+#ifdef CONFIG_KSU_SUSFS
+	__attribute__((hot))
+	extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
+					int *flags);
+
+	extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
+	#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+		extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr); // optional
+	#endif
+#endif
+
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
 SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 		struct stat __user *, statbuf, int, flag)
 {
 	struct kstat stat;
 	int error;
+
+	#ifdef CONFIG_KSU_SUSFS
+		ksu_handle_stat(&dfd, &filename, &flag);
+	#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
@@ -393,6 +408,10 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
+
+	#ifdef CONFIG_KSU_SUSFS
+		ksu_handle_newfstat_ret(&fd, &statbuf);
+	#endif
 
 	return error;
 }
@@ -513,7 +532,9 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
-
+	#ifdef CONFIG_KSU_SUSFS // for 32-bit
+		ksu_handle_fstat64_ret(&fd, &statbuf);
+	#endif
 	return error;
 }
 
@@ -522,6 +543,10 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+
+	#ifdef CONFIG_KSU_SUSFS // 32-bit su
+		ksu_handle_stat(&dfd, &filename, &flag);
+	#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
